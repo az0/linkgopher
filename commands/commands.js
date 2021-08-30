72b9200ed4491c84af7dfa56580429ae33d805a4
Andrew Ziem <ahz001@gmail.com>
2021-08-30 10:01:13 -0600

    Fix broken extension on Google Chrome
    Closes https://github.com/az0/linkgopher/issues/46

--- commands/commands.js
+++ commands/commands.js
@@ -2,12 +2,15 @@
 
 import { handler } from '../popup/js/functions.js';
 
-browser.commands.onCommand.addListener(function (command) {
-    if (command === "extract-all") {
-      handler(false);
-    }
 
-    if (command === "extract-some") {
-      handler(true);
-    }
-  });
+if (typeof browser !== 'undefined') {
+    browser.commands.onCommand.addListener(function (command) {
+        if (command === "extract-all") {
+          handler(false);
+        }
+
+        if (command === "extract-some") {
+          handler(true);
+        }
+      });
+}
